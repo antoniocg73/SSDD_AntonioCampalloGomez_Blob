5d8a7d4a42231763aa8e689e394b9e073872bafe
Antonio Campallo Gómez <ancago4@gmail.com>
2023-12-07 09:32:28 +0100

    return 0 en client

--- icedrive_blob/client.py
+++ icedrive_blob/client.py
@@ -14,6 +14,7 @@ class ClientApp(Ice.Application):
         base = self.communicator().stringToProxy("BlobService -t -e 1.1:tcp -h 127.0.0.1 -p 2000 -t 60000")
         blob = IceDrive.BlobServicePrx.checkedCast(base)
         blob.link("pruebaLink")
+        return 0
 
 def client():
     """Run the client."""
